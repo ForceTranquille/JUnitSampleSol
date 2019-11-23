f3542da8ba7f8fa6deff947bf163ae743b092434
Force Tranquille <rabi.marhoum@outlook.com>
2019-11-23 03:35:11 -0500

    classe principale

--- src/main/java/sample/AppCtr.java
+++ src/main/java/sample/AppCtr.java
@@ -5,16 +5,16 @@
  */
 package sample;
 
-
-
-
 /**
  *
  * @author marhoum
  */
 public class AppCtr {
-        public static void main(String[] args) {
-            String resultat = sample.Utils.computeFactorial(7);
-            System.out.println("Traitement" + resultat);
-        } 
+
+    public static void main(String[] args) {
+        String variable1 = args[0];
+        String variable2 = args[1];
+        args[3] = sample.Utils.concatWords(variable1 + variable2);
+        System.out.println("Resultat" + args[3]);
+    }
 }
